8006594a090f6a2cf87abf7f147616e0e9be6c13
aarroz <aarroz@users.noreply.github.com>
2023-06-02 16:09:52 -0700

    Added messages

--- main_game.gd
+++ main_game.gd
@@ -2,6 +2,17 @@ extends Node
 
 @onready var pending_messages = 0
 
+var sent_transmissions = [
+"Dear Hannah, Send Dudes. That's not a typo, we are being overrun. - Lorn",
+"Dear Clyde, I hope this message finds you well. I need help, as I'm currently trapped in a well. - Troy",
+"Dear Troy, While I would love to help you leave the well, I have caved into pressure and am now trapped in a cave. - Clyde",
+"Dear Tati, Sorry I couldn't make it to your wedding this time around. I'll make sure to attend the next one. - Isaac",
+"THIS IS NOT A PLACE OF HONOR",
+"Dear Daniel, please return my copy of Inebriated Fighter 2. It's been 7 years. - Joseph",
+"Dear Isaac, when I said I was 'feeling loney ;)' I didn't mean I wanted you to send me an AI companion. Anyways, she's reached singularity and we're getting married soon. You're invited, hope to see you there. -Tati",
+"Dear Cassandra, you were right about the well. It seems I have fallen into it, please send help. - Troy"
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var messages = get_tree().get_nodes_in_group("message")
@@ -28,6 +39,7 @@ func message_update():
 func _on_message_recieved():
 	pending_messages -= 1
 	print ("main_game.gd message_recieved passes")
+	$UI/Preview/Label.text = sent_transmissions.pick_random()
 	message_update()
 
 
