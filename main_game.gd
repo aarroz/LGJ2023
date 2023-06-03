a174ecd06955427feb8ee9e3c4a4c8686692595e
aarroz <aarroz@users.noreply.github.com>
2023-06-02 20:09:28 -0700

    Finalized end game, ready to submit

--- main_game.gd
+++ main_game.gd
@@ -9,7 +9,7 @@ var sent_transmissions = [
 "To Tati: Sorry I couldn't make it to your wedding this time around. I'll make sure to attend the next one. - Isaac",
 "THIS IS NOT A PLACE OF HONOR",
 "To Daniel: Please return my copy of Inebriated Fighter 2. It's been 7 years. - Joseph",
-"To Isaac: When I said I was 'feeling loney ;)' I didn't mean I wanted you to send me an AI companion. Anyways, she has reached singularity and we're getting married soon. You're invited, we hope to see you there. -Tati",
+"To Isaac: When I said I was 'feeling loney ;)' I didn't mean I wanted you to send me an AI companion. Anyways, she has gained sentience and we're getting married soon. You're invited, we hope to see you there. -Tati",
 "To Cassandra: You were right about the well. It seems I have fallen into it, please send help. - Troy",
 "PUBLIC: A/S/L? - Anonymous Sender",
 "To Jorge: Please reset our simulation. Everything went wrong. It no longer rains water, only teeth. It no longer shines sunlight, only heat seeps through the dark-gray sky. The air is too thick to breath and it feels like it's 315K. Worst of all, my air conditioner broke and I ran out of content to consume. Spare us. - Anonymous",
@@ -34,12 +34,7 @@ func message_update():
 	$UI/Messages/Label.text = str(pending_messages) + " pending"
 	$UI/Pop_Up.hide()
 	if pending_messages == 0:
-		print("finished")
-		$UI/Pop_Up/Congrats.text = "You won!"
-		$UI/Pop_Up/Restart.text = "Play again?"
-		$UI/Pop_Up/Quit.text = "Exit"
-		$UI/Pop_Up.show()
-		$Win.play()
+		$UI/Messages/Label.text = "Return to Base"
 
 
 func _on_message_recieved():
@@ -66,3 +61,13 @@ func _on_rocket_fuel_empty():
 
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
+
+
+func _on_finish_body_entered(body):
+	if pending_messages == 0:
+		print("finished")
+		$UI/Pop_Up/Congrats.text = "You won!"
+		$UI/Pop_Up/Restart.text = "Play again?"
+		$UI/Pop_Up/Quit.text = "Exit"
+		$UI/Pop_Up.show()
+		$Win.play()
